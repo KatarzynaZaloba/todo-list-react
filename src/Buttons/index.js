772a9588eae35ea94c47c469ea28cc55ed0c8d98
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-02-17 14:12:49 +0100

    code formatted

--- src/Buttons/index.js
+++ src/Buttons/index.js
@@ -3,22 +3,18 @@ import "./style.css";
 
 const Buttons = ({ tasks, hideDoneTasks }) => (
     <div className="buttons">
-            {tasks.lenght > 0 && (
-        <React.Fragment>
+        {tasks.lenght > 0 && (
+            <React.Fragment>
             <button className="buttons__button">
                 {hideDoneTasks ? "Pokaż" : "Ukryj"} ukończone
             </button>
-            <button
-                className="buttons__button"
-                disabled={tasks.every(({ done }) => done)}
-            >
+            <button className="buttons__button" disabled={tasks.every(({ done }) => done)}>
                 Ukończ wszystkie
             </button>
-        </React.Fragment>
-            )}
-    </div>
-
-            
+            </React.Fragment>
+            )
+        }
+    </div>   
 );
 
 export default Buttons;
