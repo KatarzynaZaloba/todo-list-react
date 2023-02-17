40b15b30eafe669cf7829889b81266d2e1811665
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-02-17 12:08:22 +0100

    hello function removed

--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import Buttons from "./Buttons";
 import Section from "./Section";
 import Header from "./Header";
 import Container from "./Container";
-import * as hello from './utils/hello';
-
-hello.hello();
-console.log(hello.name);
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
