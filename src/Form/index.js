ad785dd1224c4b61aa5ffee15e395988bdc1992d
KatarzynaZaloba <kzalobafrontend@gmail.com>
2023-02-21 14:36:43 +0100

    Button colors changed and formatted

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import "./style.css";
+import "./style.css"
 
-const Form = ({addNewTask}) => {
- const [newTaskContent, setNewTaskContent] = useState("");
+const Form = ({ addNewTask }) => {
+    const [newTaskContent, setNewTaskContent] = useState("");
 
- const onFormSubmit = (event) => {
-    event.preventDefault();
-    addNewTask(newTaskContent.trim());
-    setNewTaskContent("");
- };
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        addNewTask(newTaskContent.trim());
+        setNewTaskContent("");
+    };
 
     return (
-            <form className="form" onSubmit={onFormSubmit}>
-                <input value={newTaskContent} className="form__input" placeholder="Co jest do zrobienia?" onChange={(event) => setNewTaskContent(event.target.value)} />
-                <button className="form__button">Dodaj zadanie</button>
-            </form>
-        
-    )
+        <form className="form" onSubmit={onFormSubmit}>
+            <input
+                value={newTaskContent}
+                className="form__input"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
+            <button className="form__button">Dodaj zadanie</button>
+        </form>
+    );
 };
 
 export default Form;
